0f245bf0f3a746aacff86f14056529682aefd443
Damra <kocdamra@gmail.com>
2021-10-25 13:06:54 +0300

    Updated satellite detail textview size

--- app/src/main/java/github/damra/android/satellite/util/Extensions.kt
+++ app/src/main/java/github/damra/android/satellite/util/Extensions.kt
@@ -1,10 +1,11 @@
 package github.damra.android.satellite.util
 
-import android.text.Html
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
 
 infix fun <T> Boolean.then(param: () -> T): T? = if (this) param() else null
 
 fun TextView.fromHtml(html: String) {
-    text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
+    text = HtmlCompat.fromHtml(html, FROM_HTML_MODE_COMPACT)
 }
